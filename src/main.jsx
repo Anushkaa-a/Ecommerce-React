4eb5f1d0b9118909e75290036e8a3f30a18ffcbc
anushka <anushka6820@gmail.com>
2025-11-05 16:52:33 +0530

    fix: correct vite base path

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <CartProvider>
-    < BrowserRouter basename={import.meta.env.BASE_URL}>
+    < BrowserRouter basename='/ecommerce-react/'>
     <App/>
     </BrowserRouter>
     </CartProvider>
